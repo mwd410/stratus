31bd1387094cec9f7c119fe93040805b1cb77957
Matt Deady <mwd410@comcast.net>
2013-09-06 13:30:30 -0400

    removed old analysis section.

--- view/analysisOverview.php
+++ view/analysisOverview.php
@@ -4,35 +4,5 @@ extendView('analysis');
 beginPartial('analysisContent');
 ?>
 
-    <script>
-        var overviewGridData = <?php echo json_encode($totals); ?>;
-    </script>
-
-    <div class="container" data-ng-controller="AnalysisOverviewController">
-        <div class="row">
-            <table id="tbl_overview"
-                   class="table table-bordered table-hover table-striped tablesorter">
-                <thead>
-                    <tr class="unselectable handCursor overview-header">
-                        <th data-ng-click="sortBy('name')">Account Name</th>
-                        <th data-ng-click="sortBy('cost')">Cost</th>
-                        <th data-ng-click="sortBy('daily.value')">Daily</th>
-                        <th data-ng-click="sortBy('weekly.value')">Weekly</th>
-                        <th data-ng-click="sortBy('monthly.value')">Monthly</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr data-ng-repeat="account in accounts | orderBy:sort.property:sort.reverse">
-                        <td><a href="/analysis/totals/{{account.id}}">{{account.name}}</a></td>
-                        <td>{{account.cost}}</td>
-                        <td dd-overview-td="{{account.daily.value}}"></td>
-                        <td dd-overview-td="{{account.weekly.value}}"></td>
-                        <td dd-overview-td="{{account.monthly.value}}"></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-
 <?php
 endPartial();
