942ecc0b9bfa0d5df85fb80cb9ad0d5f8c7bce07
Matt Deady <mwd410@comcast.net>
2013-09-11 20:18:01 -0400

    further widget design

--- view/dashboard.php
+++ view/dashboard.php
@@ -21,47 +21,61 @@ beginPartial('content');
         </li>
     </ul>
 </div>
+    <!--
 
 <div class="st-dash">
-    <div>
-        <div class="st-widget-row" style="height:400px;">
-            <div class="st-widget-column col-md-4 col-md-offset-3">
-                <div class="st-widget" style="height:100%;">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h4 class="panel-title">Widget</h4>
-                        </div>
-                        <div class="panel-body">
-                        </div>
+<div>
+
+    <div class="st-widget-row" style="height:400px;">
+        <div class="st-widget-column col-md-4 col-md-offset-3">
+            <div class="st-widget" style="height:100%;">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">Widget</h4>
+                    </div>
+                    <div class="panel-body">
                     </div>
                 </div>
             </div>
-            <div class="st-widget-column col-md-2">
-                <div class="st-widget" style="height:50%;">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h4 class="panel-title">Widget</h4>
-                        </div>
-                        <div class="panel-body">
+        </div>
+        <div class="st-widget-column col-md-2">
+            <div class="st-widget-column-dragger">
+                <div>
+                </div>
+            </div>
+            <div class="st-widget" style="height:50%;">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">Widget</h4>
+                    </div>
+                    <div class="panel-body">
 
-                        </div>
                     </div>
                 </div>
-                <div class="st-widget" style="height:50%;">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h4 class="panel-title">Widget</h4>
-                        </div>
-                        <div class="panel-body">
-
-                        </div>
+            </div>
+            <div class="st-widget" style="height:50%;">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title inline" style="display: inline-block;">Widget</h4>
+                        <button type="button"
+                                class="st-widget-btn btn btn-xs">
+                            <span class="glyphicon-move glyphicon"></span>
+                        </button>
+                        <button type="button"
+                                class="st-widget-btn btn btn-xs">
+                            <span class="glyphicon-move glyphicon"></span>
+                        </button>
+                    </div>
+                    <div class="panel-body">
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
+</div>
+    -->
+<div data-st-dash="widgets"></div>
 <?php
 
 endPartial();
