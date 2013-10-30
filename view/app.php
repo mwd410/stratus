8df7134a7317af4087b4c1e5443baf2b691a5bc4
Matt Deady <mwd410@comcast.net>
2013-10-30 09:22:32 -0400

    restructure css, added footer.

--- view/app.php
+++ view/app.php
@@ -4,7 +4,17 @@ extendView('base');
 beginPartial('body');
 ?>
 
-<div ui-view></div>
+<div class="site-wrapper">
+    <div class="site-content" data-ui-view></div>
+
+    <footer class="site-footer">
+        <div class="st-wrapper">
+            <span class="left">&copy; 2013 Cloudnalysis</span>
+            <span>&nbsp;</span>
+            <span class="right">Contact</span>
+        </div>
+    </footer>
+</div>
 
 <?php
 endPartial();
