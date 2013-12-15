ec6a979e7dc7630e2eae3c1a2940a3eb84b884e5
Matt Deady <mwd410@comcast.net>
2013-12-15 00:41:33 -0500

    few tweaks

--- web/js/app/chargeback/ChargebackNavCtrl.js
+++ web/js/app/chargeback/ChargebackNavCtrl.js
@@ -4,21 +4,49 @@
     ng.module('app.chargeback').controller('ChargebackNavCtrl', function($scope) {
 
         $scope.chargeback = {
-            menus : [
+            menus    : [
                 {
-                    name : 'Pivot By',
+                    name  : 'Service Provider',
                     items : [
                         {
-                            type : 'provider',
-                            name : 'Service Provider'
-                        },
-                        {
-                            type : 'type',
-                            name : 'Service Type'
+                            id       : 1,
+                            name     : 'Amazon',
+                            isActive : true,
+                            subItems : [
+                                {
+                                    id   : 1,
+                                    name : 'EC2'
+                                }
+                            ]
                         }
                     ]
                 }
-            ]
+            ],
+            accounts : {
+                '1' : {
+                    name     : 'Stratus 1',
+                    products : {
+                        '1' : {
+                            name  : 'Amazon AWS',
+                            chargebackUnit  : {
+                                id : '1',
+                                name : 'Head of Product Development',
+                                user : {
+                                    id         : '1',
+                                    first_name : 'Matt',
+                                    last_name  : 'Deady',
+                                    email      : 'mwd410@comcast.net'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        $scope.isItemActive = function(item) {
+
+            return item.isActive;
         };
     });
 
